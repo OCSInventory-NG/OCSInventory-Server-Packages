9baa11e5e6ef56a21f7d2cffacea1a725578ec60
Gilles Dubois <gilles.dubois@factorfx.com>
2024-08-21 11:53:26 +0200

    feat(all): refactor of the image to handle correct python version

--- ocsinventory-backend/docker/3.0.0/entrypoint.sh
+++ ocsinventory-backend/docker/3.0.0/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-python /app/ocsinventory-backend/manage.py migrate
+chown -R ocsbackend: /app/
 
-uwsgi --ini /etc/uwsgi/apps-available/ocsinventory-backend.ini
+su - ocsbackend
+
+python3.11 -m venv /app/ocs-venv 
+
+# Get in Venv
+source /app/ocs-venv/bin/activate
+
+# virtualenv and python deps
+pip install -r /app/ocsinventory-backend/requirements.txt
+
+tail -f /dev/null
